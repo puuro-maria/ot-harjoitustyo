b244eb1c6b73e21f7afb758264468bd7fe3717ee
Viivi Puuronen <viivi.puuronen@hotmail.com>
2020-03-22 18:42:16 +0200

    Unicafe-projekti ja maksupäätteen testit tehty

--- laskarit/viikko2/Unicafe/src/main/java/com/mycompany/unicafe/Paaohjelma.java
+++ laskarit/viikko2/Unicafe/src/main/java/com/mycompany/unicafe/Paaohjelma.java
@@ -5,7 +5,10 @@ public class Paaohjelma {
     public static void main(String[] args) {
         Kassapaate unicafeExactum = new Kassapaate();
         Maksukortti kortti = new Maksukortti(10000);
-        
+        Maksukortti kortti2 = new Maksukortti(10);
+        System.out.println(kortti2);
+        kortti2.otaRahaa(5);
+        System.out.println(kortti2);
         unicafeExactum.syoEdullisesti(kortti);
         
         System.out.println( unicafeExactum.edullisiaLounaitaMyyty() );
--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,33 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void saldoAlussaOikein(){
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    @Test
+    public void lataaminenKasvattaaSaldoa(){
+        kortti.lataaRahaa(1000);
+        assertEquals("saldo: 10.10", kortti.toString());
+    }
+    
+    @Test
+    public void saldoVahenee(){
+        kortti.otaRahaa(5);
+        assertEquals("saldo: 0.05", kortti.toString());
+    }
+    
+    @Test
+    public void saldoEiVaheneJosOttaaLiikaa(){
+        kortti.otaRahaa(200);
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    @Test
+    public void otaRahaaPalauttaaOikein(){
+        assertTrue(kortti.otaRahaa(5));
+        assertFalse(kortti.otaRahaa(200));
+    }
 }
